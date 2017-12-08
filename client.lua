25af6687aa35e089a4dcf691917db8e1c38b678f
iEns <github@iens.dk>
2017-12-08 09:06:59 +0100

    Complete rewrite of the damage algorithm to prevent easy explosions from shooting the fuel tank of a damaged vehicle Fix for inability to repair vehicle sometimes Fix for incompatibility with other /fix scripts and fix using trainers Added option for explosion prevention. Will not be effective 100% of the time, but it's way better than nothing.

--- client.lua
+++ client.lua
@@ -9,10 +9,10 @@
 --	https://github.com/iEns/RealisticVehicleFailure
 --
 
-local damageFactor = 15.0					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 15
+local damageFactor = 8.0					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
 local cascadingFailureSpeedFactor = 10.0	-- Sane values are 1 to 100. When vehicle health drops below a certain point, cascading failure sets in, and the health drops rapidly until the vehicle dies. Higher values means faster failure. A good starting point is 10
 local displayBlips = true					-- Show blips for mechanics locations
-
+local preventExplosions = true				-- If true, most explosions will be prevented while in the vehicle
 
 
 
@@ -32,14 +32,15 @@ local mechanics = {
 	{name="Mechanic", id=446, r=22.0, x=484.0,   y=-1316.0, z=29.0},	-- Hayes Auto, Little Bighorn Ave.
 	{name="Mechanic", id=446, r=30.0, x=-1419.0, y=-450.0,  z=36.0},	-- Hayes Auto Body Shop, Del Perro
 	{name="Mechanic", id=446, r=30.0, x=268.0,   y=-1810.0, z=27.0},	-- Hayes Auto Body Shop, Davis
+--	{name="Mechanic", id=446, r=20.0, x=288.0,   y=-1730.0, z=29.0},	-- Hayes Auto, Rancho (Disabled, looks like a warehouse for the Davis branch)
 	{name="Mechanic", id=446, r=25.0, x=1915.0,  y=3729.0,  z=32.0},	-- Otto's Auto Parts, Sandy Shores
 	{name="Mechanic", id=446, r=45.0, x=-29.0,   y=-1665.0, z=29.0},	-- Mosley Auto Service, Strawberry
 	{name="Mechanic", id=446, r=42.0, x=-212.0,  y=-1378.0, z=31.0},	-- Glass Heroes, Strawberry
 	{name="Mechanic", id=446, r=30.0, x=258.0,   y=2594.0,  z=44.0},	-- Mechanic Harmony
 	{name="Mechanic", id=446, r=15.0, x=-32.0,   y=-1090.0, z=26.0},	-- Simeons
 	{name="Mechanic", id=446, r=20.0, x=-211.0,  y=-1325.0, z=31.0},	-- Bennys
-	{name="Mechanic", id=446, r=20.0, x=903.0,  y=3563.0, z=34.0},		-- Auto Repair, Grand Senora Desert
-	{name="Mechanic", id=446, r=20.0, x=437.0,  y=3568.0, z=38.0}		-- Auto Shop, Grand Senora Desert
+	{name="Mechanic", id=446, r=20.0, x=903.0,   y=3563.0,  z=34.0},	-- Auto Repair, Grand Senora Desert
+	{name="Mechanic", id=446, r=20.0, x=437.0,   y=3568.0,  z=38.0}		-- Auto Shop, Grand Senora Desert
 }
 
 local fixMessages = {
@@ -67,14 +68,15 @@ local noFixMessagePos = math.random(noFixMessageCount)
 
 local healthEngineLast = 9999.9
 local healthBodyLast = 9999.9
-local healthPetrolLast = 9999.9
+local healthPetrolTankLast = 9999.9
 local healthEngineCurrent = 1000.0
 local healthEngineNew = 1000.0
 local healthBodyCurrent = 1000.0
 local healthBodyNew = 1000.0
+local healthBodyDelta = 0.0
 local healthPetrolTankCurrent = 1000.0
 local healthPetrolTankNew = 1000.0
-
+local healthPetrolTankDelta = 0.0
 
 -- Display blips on map
 Citizen.CreateThread(function()
@@ -97,20 +99,22 @@ AddEventHandler('iens:repair', function()
 		local vehicle = GetVehiclePedIsIn(ped, false)
 		if IsNearMechanic() then
 			SetVehicleUndriveable(vehicle,false)
-			SetVehicleEngineHealth(vehicle, 1000.0)
-			SetVehicleBodyHealth(vehicle, 1000.0)
-			SetVehiclePetrolTankHealth(vehicle, 1000.0)
 			SetVehicleFixed(vehicle)
+			healthBodyLast=1000.0
+			healthEngineLast=1000.0
+			healthPetrolTankLast=1000.0
 			SetVehicleEngineOn(vehicle, true, false )
 			notification("~g~The mechanic repaired your car!")
 			return
 		end
-		if GetVehicleEngineHealth(vehicle) < 2 and GetVehiclePetrolTankHealth(vehicle) < 2 then
+		if GetVehicleEngineHealth(vehicle) < 2 then
 			if GetVehicleOilLevel(vehicle) > 0 then
 				SetVehicleUndriveable(vehicle,false)
 				SetVehicleEngineHealth(vehicle, 305.0)
-				SetVehiclePetrolTankHealth(vehicle, 703.0)
-				SetVehicleEngineOn(vehicle, true, false )
+				SetVehiclePetrolTankHealth(vehicle, 750.0)
+				healthEngineLast=305.0
+				healthPetrolTankLast=750.0
+					SetVehicleEngineOn(vehicle, true, false )
 				SetVehicleOilLevel(vehicle,(GetVehicleOilLevel(vehicle)/3)-0.5)
 				notification("~g~" .. fixMessages[fixMessagePos] .. ", now go to a mechanic!")
 				fixMessagePos = fixMessagePos + 1
@@ -157,38 +161,36 @@ Citizen.CreateThread(function()
 		if IsPedInAnyVehicle(ped, false) then
 			vehicle = GetVehiclePedIsUsing(ped)
 			healthEngineCurrent = GetVehicleEngineHealth(vehicle)
+			if healthEngineCurrent == 1000 then healthBodyLast = 1000.0 end
 			healthEngineNew = healthEngineCurrent
 			healthBodyCurrent = GetVehicleBodyHealth(vehicle)
+			if healthBodyCurrent == 1000 then healthBodyLast = 1000.0 end
 			healthBodyNew = healthBodyCurrent
 			healthPetrolTankCurrent = GetVehiclePetrolTankHealth(vehicle)
+			if healthPetrolTankCurrent == 1000 then healthPetrolTankLast = 1000.0 end
 			healthPetrolTankNew = healthPetrolTankCurrent 
 			if healthEngineCurrent > 2 and healthPetrolTankCurrent > 2 then
 				SetVehicleUndriveable(vehicle,false)
 			end
-			if healthEngineCurrent < 2 and healthPetrolTankCurrent < 2 then
+
+			if healthEngineCurrent < 2 then
 				SetVehicleUndriveable(vehicle,true)
 			elseif healthEngineLast ~= 9999.9 then
 				if healthEngineLast - healthEngineCurrent > 0 then
 					healthEngineNew = healthEngineLast - ((healthEngineLast - healthEngineCurrent) * damageFactor)
 				end
+				if healthEngineNew < 300 then
+					healthEngineNew = healthEngineNew-(0.1 * cascadingFailureSpeedFactor)
+				end
 				if healthEngineNew < 1 then
 					healthEngineNew = 1.0
 				end
-				SetVehicleEngineHealth(vehicle, healthEngineNew)
-				healthEngineLast = healthEngineNew
-				if healthEngineNew < 300 then
-					local t = GetVehiclePetrolTankHealth(vehicle)-(0.01 * cascadingFailureSpeedFactor)
-					if t < 0 then
-						t = 0
-					end
-					SetVehiclePetrolTankHealth(vehicle, t) 
-				end
-			else
-				healthEngineLast = GetVehicleEngineHealth(vehicle)
 			end
+
 			if healthBodyLast ~= 9999.9 then
-				if healthBodyLast - healthBodyCurrent > 0 then
-					healthBodyNew = healthBodyLast - ((healthBodyLast - healthBodyCurrent) * 10.0)
+				healthBodyDelta = healthBodyLast - healthBodyCurrent
+				if healthBodyDelta > 0 then
+					healthBodyNew = healthBodyLast - ((healthBodyLast - healthBodyCurrent) * damageFactor)
 				end
 				if healthBodyNew < 0 then
 					healthBodyNew = 0.0
@@ -196,33 +198,35 @@ Citizen.CreateThread(function()
 				SetVehicleBodyHealth(vehicle, healthBodyNew)
 				healthBodyLast = healthBodyNew
 			else
-				healthBodyLast = GetVehicleBodyHealth(vehicle)
+				if healthBodyCurrent < 300 then
+					SetVehicleBodyHealth(vehicle,300.0)
+					healthBodyCurrent=300.0
+					healthBodyLast=300.0
+				end
 			end
-			if healthPetrolLast ~= 9999.9 then
-				if healthPetrolLast - healthPetrolTankCurrent > 0 then
-					healthPetrolTankNew = healthPetrolLast - ((healthPetrolLast - healthPetrolTankCurrent ) * damageFactor * 5)
+
+			if healthPetrolTankLast ~= 9999.9 then
+				healthPetrolTankDelta = healthPetrolTankLast-healthPetrolTankCurrent
+				if healthPetrolTankDelta > 0 then
+					healthPetrolTankLast = healthPetrolTankCurrent
 				end
-				if healthPetrolTankNew < 1 then
-					healthPetrolTankNew = 1.0
+				if healthPetrolTankCurrent < 750 and preventExplosions == true then
+					SetVehiclePetrolTankHealth(vehicle, 750.0)
+					healthPetrolTankLast = 750
 				end
-				if healthPetrolTankNew ~= healthPetrolLast then
-					SetVehiclePetrolTankHealth(vehicle, healthPetrolTankNew)
-				end
-				healthPetrolLast = healthPetrolTankNew
-				if healthPetrolTankNew < 702 then
-					local t = GetVehicleEngineHealth(vehicle)-(0.05 * cascadingFailureSpeedFactor)
-					if t < 1 then
-						t = 1
-					end
-					SetVehicleEngineHealth(vehicle, t) 
-				end
-			else
-				healthPetrolLast = GetVehiclePetrolTankHealth(vehicle)
 			end
+
+			healthEngineNew = healthEngineNew - (healthPetrolTankDelta * damageFactor * 8) - (healthBodyDelta * damageFactor)
+			if healthEngineNew < 1 then healthEngineNew = 1.0 end
+			SetVehicleEngineHealth(vehicle, healthEngineNew)
+
+			healthEngineLast = healthEngineNew
+			healthBodyLast = GetVehicleBodyHealth(vehicle)
+			healthPetrolTankLast = GetVehiclePetrolTankHealth(vehicle)
 		else
 			healthEngineLast = 9999.9
 			healthBodyLast = 9999.9
-			healthPetrolLast = 9999.9
+			healthPetrolTankLast = 9999.9
 		end
 	end
 end)
